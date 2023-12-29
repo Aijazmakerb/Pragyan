8131814a5b2feca2a470cb33d9f0bbb8a282c2b8
A i j a Z <72906651+Aijazmakerb@users.noreply.github.com>
2023-12-29 15:47:06 +0530

    Final

--- Pragyan.ino
+++ Pragyan.ino
@@ -1,6 +1,5 @@
 #include <NewPing.h>
-//W for hand tracking
-//w for remote control
+
 #define TRIG_PIN 12
 #define ECHO_PIN 11
 
@@ -11,7 +10,7 @@ const int pin3 = 7;
 const int pin4 = 6;
 const int en2 = 10;
 
-const int speed = 130;
+int speed = 80;
 const int turnSpeed = 150;
 
 const int ir1 = 2;
@@ -19,7 +18,7 @@ const int ir2 = 3;
 
 char command;
 
-int mode = 1;
+int mode;
 
 NewPing sonar(TRIG_PIN, ECHO_PIN);
 
@@ -35,6 +34,8 @@ void setup() {
 
   pinMode(ir1, INPUT);
   pinMode(ir2, INPUT);
+
+  mode = 1;
 }
 
 void loop() {
@@ -43,14 +44,20 @@ void loop() {
     command = Serial.read();
     Serial.println(command);
   }
- 
+
   if (command == 'W') {
     mode = 1;
+    speed = 140;
   } else if (command == 'w') {
     mode = 2;
+    speed = 115;
+  } else if (command == 'o') {
+    mode = 3;
+    speed = 80;
   }
 
   int distance = sonar.ping_cm();
+  delay(50);
 
   int irLeft = digitalRead(ir1);
   int irRight = digitalRead(ir2);
@@ -66,16 +73,49 @@ void loop() {
       backward();
     } else if (command == 'S') {
       stop();
+    } else if (command == 'f' && distance < 75) {
+      forward();
+      delay(2500);
+      stop();
+      command = 'S';
+    } else if (command == 'l') {
+      aleft();
+      command = 'S';
+    } else if (command = 'r') {
+      aright();
+      command = 'S';
+    } else if (command == 'b') {
+      backward();
+      delay(2000);
+      stop();
+      command = 'S';
     }
   } else if (mode == 2) {
-    if (distance >= 1 && distance <= 20) {
+    if (distance >= 8 && distance <= 20) {
       forward();
     } else if (irLeft == LOW && irRight == HIGH) {
       left();
     } else if (irLeft == HIGH && irRight == LOW) {
       right();
+    } else if (distance > 0 && distance < 5) {
+      backward();
     } else {
       stop();
+    }
+  } else if (mode == 3) {
+    if (distance > 20) {
+      forward();
+    } else if (distance < 20) {
+      backward();
+      delay(500);
+      stop();
+      delay(200);
+      int var = randomLeftOrRight();
+      if (var == 1) {
+        aleft();
+      } else {
+        aright();
+      }
     }
   }
 }
@@ -102,13 +142,13 @@ void right() {
   digitalWrite(pin2, LOW);
   digitalWrite(pin1, HIGH);
   digitalWrite(pin3, LOW);
-  digitalWrite(pin4, LOW);
+  digitalWrite(pin4, HIGH);
   analogWrite(en1, turnSpeed);
   analogWrite(en2, turnSpeed);
 }
 
 void left() {
-  digitalWrite(pin2, LOW);
+  digitalWrite(pin2, HIGH);
   digitalWrite(pin1, LOW);
   digitalWrite(pin3, HIGH);
   digitalWrite(pin4, LOW);
@@ -121,4 +161,47 @@ void stop() {
   digitalWrite(pin1, LOW);
   digitalWrite(pin3, LOW);
   digitalWrite(pin4, LOW);
+}
+
+void aleft() {
+  digitalWrite(pin2, HIGH);
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin3, HIGH);
+  digitalWrite(pin4, LOW);
+  analogWrite(en1, turnSpeed);
+  analogWrite(en2, turnSpeed);
+  delay(1000);
+  digitalWrite(pin2, LOW);
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin3, LOW);
+  digitalWrite(pin4, LOW);
+}
+
+void aright() {
+  digitalWrite(pin2, LOW);
+  digitalWrite(pin1, HIGH);
+  digitalWrite(pin3, LOW);
+  digitalWrite(pin4, HIGH);
+  analogWrite(en1, turnSpeed);
+  analogWrite(en2, turnSpeed);
+  delay(1000);
+  digitalWrite(pin2, LOW);
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin3, LOW);
+  digitalWrite(pin4, LOW);
+}
+
+void aforward() {
+  digitalWrite(pin1, HIGH);
+  digitalWrite(pin3, HIGH);
+  digitalWrite(pin2, LOW);
+  digitalWrite(pin4, LOW);
+  analogWrite(en1, speed);
+  analogWrite(en2, speed);
+  delay(1000);
+}
+
+int randomLeftOrRight() {
+  int randomNumber = random(2);
+  return randomNumber;
 }
